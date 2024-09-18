bed581e496a7e2993845f160ce33b79b4ca6891a
mohamed lahlaili <100442993+lahlaili@users.noreply.github.com>
2024-09-18 12:32:27 +0100

    Add files via upload

---
+++ odoo store/work/identifiants_fiscaux_marocains/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Identifiants fiscaux marocains",
+
+    'summary': """
+        identifiants fiscaux marocains""",
+
+    'description': """
+       
+    """,
+
+
+    'category': 'purchase',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+
+      
+        
+        'views/view.xml'
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+}
